0a0b441fafbd6dc27773690667d8fc643678a9a5
Kornél Kiss <82728993+kisskornel@users.noreply.github.com>
2023-03-16 18:14:02 +0100

    Main Files for building the Server.
    Main Files for building the Server. Or pull the image from docker hub.

---
+++ page_default/error500.php
@@ -0,0 +1,194 @@
+<?php
+$IP=$_SERVER['REMOTE_ADDR'];
+$SERVER_TYPE='Apache/IKEA';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!-- saved from url=(0066)/ -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><script type="text/javascript" src="./Page_style/bundle-playback.js.download" charset="utf-8"></script><style type="text/css" id="operaUserStyle"></style><script>(function(){class RuffleMimeType{constructor(a,b,c){this.type=a,this.description=b,this.suffixes=c}}class RuffleMimeTypeArray{constructor(a){this.__mimetypes=[],this.__named_mimetypes={};for(let b of a)this.install(b)}install(a){let b=this.__mimetypes.length;this.__mimetypes.push(a),this.__named_mimetypes[a.type]=a,this[a.type]=a,this[b]=a}item(a){return this.__mimetypes[a]}namedItem(a){return this.__named_mimetypes[a]}get length(){return this.__mimetypes.length}}class RufflePlugin extends RuffleMimeTypeArray{constructor(a,b,c,d){super(d),this.name=a,this.description=b,this.filename=c}install(a){a.enabledPlugin||(a.enabledPlugin=this),super.install(a)}}class RufflePluginArray{[Symbol.iterator](){return this.__plugins[Symbol.iterator]()}constructor(a){this.__plugins=[],this.__named_plugins={};for(let b of a)this.install(b)}install(a){let b=this.__plugins.length;this.__plugins.push(a),this.__named_plugins[a.name]=a,this[a.name]=a,this[b]=a}item(a){return this.__plugins[a]}namedItem(a){return this.__named_plugins[a]}refresh(){}get length(){return this.__plugins.length}}const FLASH_PLUGIN=new RufflePlugin("Shockwave Flash","Shockwave Flash 32.0 r0","ruffle.js",[new RuffleMimeType("application/futuresplash","Shockwave Flash","spl"),new RuffleMimeType("application/x-shockwave-flash","Shockwave Flash","swf"),new RuffleMimeType("application/x-shockwave-flash2-preview","Shockwave Flash","swf"),new RuffleMimeType("application/vnd.adobe.flash.movie","Shockwave Flash","swf")]);function install_plugin(a){navigator.plugins.install||Object.defineProperty(navigator,"plugins",{value:new RufflePluginArray(navigator.plugins),writable:!1}),navigator.plugins.install(a),0<a.length&&!navigator.mimeTypes.install&&Object.defineProperty(navigator,"mimeTypes",{value:new RuffleMimeTypeArray(navigator.mimeTypes),writable:!1});for(var b=0;b<a.length;b+=1)navigator.mimeTypes.install(a[b])}install_plugin(FLASH_PLUGIN);})();</script><script src="chrome-extension://donbcfbmhbcapadipfkeojnmajbakjdc/dist/ruffle.js?id=10455233774"></script>
+<script type="text/javascript" src="/Page_style/wombat.js.download" charset="utf-8"></script>
+<script type="text/javascript">
+  __wm.init("https://web.archive.org/web");
+  __wm.wombat("http://secure.ikea.com/","20090727220855","https://web.archive.org/","web","/_static/",
+	      "1248732535");
+</script>
+<link rel="stylesheet" type="text/css" href="/Page_style/banner-styles.css">
+<link rel="stylesheet" type="text/css" href="/Page_style/iconochive.css">
+<link rel="stylesheet" type="text/css" href="/Page_style/css.css">
+<!-- End Wayback Rewrite JS Include -->
+
+	<title>IKEA - This site is unavailable</title>
+	
+	<link href="/Page_style/global.css" type="text/css" rel="stylesheet">
+	<link href="/Page_style/temp10.css" type="text/css" rel="stylesheet">
+	<meta content="noindex, nofollow" name="robots">
+	<meta content="no" name="searchable">
+</head>
+<body id="nav01_body" bgcolor="#ffffff" leftmargin="0" topmargin="0" onload="" marginwidth="0" marginheight="0"><!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<style type="text/css">
+body {
+  margin-top:0 !important;
+  padding-top:0 !important;
+  /*min-width:800px !important;*/
+}
+</style>
+<script>__wm.rw(0);</script>
+<div id="wm-ipp-base" lang="en" style="display: block; direction: ltr; height: 1px;">
+</div><div id="wm-ipp-print">The Wayback Machine - /</div>
+<script type="text/javascript">//<![CDATA[
+__wm.bt(700,27,25,2,"web","http://secure.ikea.com/","20090727220855",1996,"/_static/",["/_static/css/banner-styles.css?v=S1zqJCYt","/_static/css/iconochive.css?v=qtvMKcIJ"], false);
+  __wm.rw(1);
+//]]></script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+<div class="margin">
+<table cellspacing="0" cellpadding="0" width="571" border="0">
+  <tbody><tr>
+    <td height="12"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+  </tr>
+  <tr>
+    <td valign="bottom" width="571"><span class="orangeHeader">This secure site is unavailable</span></td>
+  </tr>
+  <tr>
+    <td height="9"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+  </tr>
+</tbody></table>
+<table id="temp10_01" cellspacing="0" cellpadding="0" width="560" border="0">
+  <tbody><tr>
+    <td class="leftSeparator" bgcolor="#ffffff"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+    <td class="rightSeparator" bgcolor="#ffffff"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+  </tr>
+  <tr>
+    <td class="grey5" valign="top">
+      <div class="leftText">
+	      <b>This site is unavailable<br>This site is being updated and is currently unavailable.<br>Please, visite us again soon!</b>
+	      <br><br>This text is also available in:<br>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#da">Dansk</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#de">Deutsch</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#fr">Français</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#es">Español</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#pt">Português</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#cs">Češtině</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#fi">Suomen kieli</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#hu">Magyar</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#it">Italiano</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#ni">Nederlands</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#pi">Polsku</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#ru">Русский</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#sk">Slovinský</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#sv">Svenska</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#no">Norsk</a>
+	      <br><img height="6" alt="" src="/Page_style/arrowred8x6.gif" width="8" border="0">&nbsp;<a class="Link1" href="/#ch">中文</a>
+	  </div></td>
+    <td class="rightImage" valign="top" align="right"><img height="279" alt="" src="/Page_style/ERROR_25.JPG" width="279" border="0"></td>
+  </tr>
+  <tr>
+    <td class="leftSeparator" bgcolor="#ffffff"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+    <td class="rightSeparator" bgcolor="#ffffff"><img height="1" alt="" src="/Page_style/s1x1.gif" width="1" border="0"></td>
+  </tr>
+</tbody></table>
+<br>
+<a name="da"><span class="grey5" valign="top"><b>DANSK</b><br>
+      Denne side er ikke tilgængelig.<br>
+      Siden opdateres og er iøjeblikket ikke tilgængelig.
+      Venligst, besøg os snarligt igen!
+</span>
+<br><br>
+</a><a name="de"><span class="grey5" valign="top"><b>DEUTSCH</b><br>
+      Diese Seite ist derzeit nicht verfügbar.<br>
+      Aufgrund von Wartungsarbeiten ist die Seite derzeit leider nicht verfügbar.<br>
+      Bitte versuchen Sie es in Kürze wieder.
+</span>
+<br><br>
+</a><a name="fr"><span class="grey5" valign="top"><b>FRANÇAIS</b><br>
+      Ce site n'est pas disponible! <br>
+      Ce site est en phase de maintenance et momentanément pas disponible.<br>
+      Rendez-nous visite sous peu! 
+</span>
+<br><br>
+</a><a name="es"><span class="grey5" valign="top"><b>ESPAÑOL</b><br>
+      La web no se encuentra disponible<br>
+      La web está siendo actualizada y no está disponible en este momento.<br>
+      Pronto, podrás volver a visitarnos!
+</span>
+<br><br>
+</a><a name="pt"><span class="grey5" valign="top"><b>PORTUGUÊS</b><br>
+      O site não se encontra disponível.<br>
+      Este site está a ser actualizado neste momento e por esta razão não está disponível.<br>
+      Não deixe de nos visitar!
+</span>
+<br><br>
+</a><a name="cs"><span class="grey5" valign="top"><b>ČEŠTINĚ</b><br>
+	Tato stránka je nedostupná<br>
+	Tato stránka je momentálne nedostupná z duvodu údržby.<br>
+	Prosíme, navštivte tuto stránku pozdeji!
+</span>
+<br><br>
+</a><a name="fi"><span class="grey5" valign="top"><b>SUOMEN KIELI</b><br>
+	Sivuihin ei saada yhteyttä <br>
+	Näihin sivuihin ei saada yhteyttä päivityksen takia. <br>
+	Yritä hetken päästä uudelleen! 
+</span>
+<br><br>
+</a><a name="hu"><span class="grey5" valign="top"><b>MAGYAR</b><br>
+	Ez az oldal nem elérheto <br>
+	Átalakítás miatt az oldal jelenleg nem elérheto. <br>
+	Kérjük, látogass vissza késobb. 
+</span>
+<br><br>
+</a><a name="it"><span class="grey5" valign="top"><b>ITALIANO</b><br>
+	Questo sito non è disponibile!<br>
+	Per motivi di manutenzione, questo sito al momento non è disponibile. <br>
+	Tornate presto a visitarci! 
+</span>
+<br><br>
+</a><a name="ni"><span class="grey5" valign="top"><b>NEDERLANDS</b><br>
+	Deze site is niet beschikbaar! <br>
+	Omwille van onderhoudsredenen is deze site momenteel niet beschikbaar. <br>
+	Bezoek ons zo snel mogelijk opnieuw! 
+</span>
+<br><br>
+</a><a name="pi"><span class="grey5" valign="top"><b>POLSKU</b><br>
+	Nie mozna wyswietlic strony <br>
+	Z powodów technicznych strona jest czasowo niedostepna. <br>
+	Spróbuj odwiedzic nas ponownie za chwile. 
+</span>
+<br><br>
+</a><a name="ru"><span class="grey5" valign="top"><b>РУССКИЙ</b><br>
+	сайт недоступен <br>
+	По техническим причинам сайт временно недоступен. <br>
+	Приносим свои извинения за доставленные неудобства. 
+</span>
+<br><br>
+</a><a name="sk"><span class="grey5" valign="top"><b>SLOVINSKÝ</b><br>
+	Táto stránka je nedostupná <br>
+	Táto stránka je momentálne z dôvodu jej údržby nedostupná. <br>
+	Navštívte nás, prosím, čoskoro! 
+</span>
+<br><br>
+</a><a name="sv"><span class="grey5" valign="top"><b>SVENSKA</b><br>
+	Ursäkta men vi bygger om! <br>
+	På grund av underhåll så är sidan tyvärr ej tillgänglig för tillfället.<br> 
+	Välkommen åter! 
+</span>
+<br><br>
+</a><a name="no"><span class="grey5" valign="top"><b>NORSK</b><br>
+	Beklager, siden er ikke tilgjengelig for øyeblikket <br>
+	Vi beklager, på grunn av vedlikehold er denne siden ikke tilgjengelig for øyeblikket.
+</span>
+<br><br>
+</a><a name="ch"><span class="grey5" valign="top"><b>中文</b><br>
+	由于网站正处于维护过程中，请您稍后再次访问！
+</span>
+<br><br>
+</a></div><a name="ch">
+
+
+
+</a>
+<h3 class="ErrorCode">Detailed Information about the error:</h3>
+<h6 class="ErrorCode">Error Code: http500</h6>
+<h6 class="ErrorCode">Your IP Address: <?php echo $IP;?></h6>
+<h6 class="ErrorCode">Current Time: <?php echo date("Y.m.d");?></h6>
+<h6 class="ErrorCode">Server Type: <?php echo $SERVER_TYPE;?></h6>
+<h6 class="ErrorCode">User Agent: <?php echo $_SERVER['HTTP_USER_AGENT'];?></h6>
+<h6 class="ErrorCode">IKEA Web Server at Port <?php echo $_SERVER["SERVER_PORT"];?></h6>
+</body></html>
